f2c6f9b0839382d449000f40a4aadcf63b7bb50a
maikndawer <maikndawer@gmail.com>
2015-03-30 15:50:18 -0300

    se añaden: templates parciales de administracion de afiliados factory para interactuar con la REST api estilos en .less y .css

--- adunceWeb/scripts/controllers/admin.js
+++ adunceWeb/scripts/controllers/admin.js
@@ -1,5 +1,43 @@
 
 
-angular.module('adunceApp').controller('AdminController', function($scope){
+angular.module('adunceApp').controller('AdminController', function($scope, afiliados){
 	
+	$scope.visibility = true;
+	
+	$scope.initDash = function(){
+		$scope.title = 'Acciones';
+		$scope.sections = [
+		         { 'name': 'Afiliados',
+		           'actions' : [
+		                        { 'name' : 'Listar' },
+		                        { 'name' : 'Agregar' },
+		                        { 'name' : 'Borrar' },
+		                        ] 
+		        },
+		        { 'name': 'Servicios',
+			       'actions' : [
+			                    { 'name' : 'Listar' },
+			                    ] 
+			    },
+		        
+		     ];
+	}
+	
+	$scope.showSection = function(section){
+		$scope.activeSection = section;
+	}
+	
+	$scope.includeSection = function(section){
+		return "views/adminPartials/"+section+".html";
+	}
+	
+	$scope.showActions = function() {
+        $scope.visibility = $scope.visibility === false ? true: false;
+    };
+    
+    $scope.listAfiliados = function(){
+    	afiliados.getAfiliados().success(function(data){
+    		$scope.afiliados = data;
+    	});
+    }
 })
---
+++ adunceWeb/scripts/factory/afiliados.js
@@ -0,0 +1,11 @@
+
+angular.module('adunceApp').factory('afiliados', ['$http', function($http){
+	
+	var afiliados = {};
+	
+	afiliados.getAfiliados = function(){
+		return $http.get('http://dev.coodesoft.com.ar:8080/adunce/REST/afiliados')
+	};
+	
+	return afiliados;
+}])
